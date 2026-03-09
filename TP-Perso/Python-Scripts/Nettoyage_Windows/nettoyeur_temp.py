0fc2228aba9f811607a6f5db994a11c24e6799c8
JiJi <jimmy.paulin@laposte.net>
2026-03-09 13:39:00 +0000

    Update nettoyeur_temp.py

--- TP-Perso/Python-Scripts/Nettoyage_Windows/nettoyeur_temp.py
+++ TP-Perso/Python-Scripts/Nettoyage_Windows/nettoyeur_temp.py
@@ -1,1 +1,30 @@
+import os
+from pathlib import Path
 
+def main():
+    total = 0
+    dossiers = [
+        os.environ["TEMP"],
+        "C:/Windows/Temp",
+        os.environ["LOCALAPPDATA"] + "/Temp",
+        os.environ["LOCALAPPDATA"] + "/LibreWolf/Cache",
+        "C:/Users/JiJi/Pictures/Screenshots",
+    ]
+    
+
+    for dossier in dossiers:
+
+        for fichier in Path(dossier).glob("*"):
+            try:
+                fichier.unlink()
+                total += 1
+            except:
+                pass
+    print(f"{total} fichiers supprimés !")
+    input("Appuyez sur Entrée")
+
+
+
+
+if __name__ == "__main__":
+    main()
